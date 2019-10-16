7309e4b186b1582f03ee88ea047b88fd91ebb094
markwhittyunsw <m.whitty@unsw.edu.au>
2019-10-17 09:53:09 +1100

    Added power function

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,4 +1,7 @@
 #include "helloworld.hpp"
 int main(int argc, char **argv) {
+	int i = 4;
+	int j = pow(i, 2);
 	std::cout << "Hello world today" << std::endl;
+	std::cout << "i = " << i << ", j = " << j << std::endl;
 }
